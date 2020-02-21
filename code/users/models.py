b130d3ba9648e6598e4c8e424c9da87f784d1d52
bataisk <donnatroal@gmail.com>
2020-02-21 03:09:58 +0200

    Added models to users app

--- code/users/models.py
+++ code/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MyUser(User):
+    picture = models.ImageField(null=True)
+    description = models.TextField(null=True)
+    rating = models.IntegerField(default=0)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='comments')
+    rated_users = models.ManyToManyField(MyUser, related_name='rated_comments')
+
+
+class CommentPhoto(models.Model):
+    picture = models.ImageField()
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
