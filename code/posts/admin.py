668162d7ca1d8676a2a990b89a5cb98e9d8b2568
Smet <dimkazhr@gmail.com>
2020-02-25 22:36:07 +0200

    Register posts models

--- code/posts/admin.py
+++ code/posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post, Rubric, PostPhoto
 
-# Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Rubric)
+admin.site.register(PostPhoto)
