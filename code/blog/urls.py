831b4d65e893327937234b3cb1e6cfd8d92952a4
bataisk <donnatroal@gmail.com>
2020-02-29 22:00:42 +0200

    Resolved merge conflict

--- code/blog/urls.py
+++ code/blog/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('posts/', include('posts.urls')),
     path('users/', include('users.urls')),
     path('api/v1/users/', include('users.api_urls')),
     path('admin/', admin.site.urls),
