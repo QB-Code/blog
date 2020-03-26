966f83149bda94699259c7dcadc0cc28e3bd50c4
Smet <dimkazhr@gmail.com>
2020-03-26 02:36:28 +0200

    Add empty views and templates

--- code/posts/urls.py
+++ code/posts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from .views import all_posts, uno_post, rubrics, write_post
 
 
 urlpatterns = [
-
+    path('', all_posts, name='all_posts'),
+    path('post/<int:post_id>/', uno_post, name='uno_post'),
+    path('write_post/<int:user_id>/', write_post, name='write_post'),
+    path('rubrics/', rubrics, name='rubrics'),
 ]
--- code/posts/views.py
+++ code/posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def uno_post(request, post_id):
+    return render(request, template_name='posts/uno-post.html')
+
+
+def all_posts(request):
+    return render(request, template_name='posts/all-posts.html')
+
+
+def write_post(request, user_id):
+    return render(request, template_name='posts/writing-post.html')
+
+
+def rubrics(request):
+    return render(request, template_name='posts/rubrics.html')
